139f368f6202ab79d615da2cf9ab3bf79b18a100
CodyandSam <37190239+CodyandSam@users.noreply.github.com>
2018-03-09 14:16:42 -0700

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,19 @@
+package lab6;
+
+public class Item {
+	
+	private String itemType;
+	private int itemValue;
+	
+	public Item(String itemType, int itemValue) {
+		this.itemType = itemType;
+		this.itemValue = itemValue; 
+		
+	}
+	
+	public String toString() {
+		return "I am an item";
+		
+	}
+
+}
