3d588242a0b045575abdf15dbf288cc26a9c92dc
CodyandSam <37190239+CodyandSam@users.noreply.github.com>
2018-03-21 14:30:52 -0600

    Add files via upload

--- Panel.java
+++ Panel.java
@@ -1,7 +1,10 @@
 package Game;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
@@ -10,44 +13,54 @@ import java.util.Scanner;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Panel extends JPanel implements KeyListener{
 	
-	player myPlayer = new player("Mario", 100, 100, "./src/Mario.png");
+	Random myRand = new Random();
+	player myPlayer = new player("Luigi", 100, 100, "./src/Luigi.png");
 	Monster myMonster = new Monster("Bowser", 200, 200, "./src/Bowser.jpg");
+	Item myItem;
 	ImageIcon myMonsterIcon = new ImageIcon(myMonster.getImagePath());
 	ImageIcon myIcon = new ImageIcon(myPlayer.getImagePath());
-
+	Timer myTimer = new Timer(500, new TimerListener());
+	
 	public Panel(){
 		setPreferredSize(new Dimension(800, 800));
 		addKeyListener(this);
 		setFocusable(true);
+		myTimer.start();
 	}
+	
 	
 	public void paintComponent(Graphics page) {
 		super.paintComponent(page);
-		page.drawImage(myIcon.getImage(), myPlayer.getX(), myPlayer.getY(), 75, 75, null);
-		page.drawImage(myMonsterIcon.getImage(), myMonster.getX(), myMonster.getY(), 100, 100, null);
+		page.drawImage(myIcon.getImage(), myPlayer.getX(), myPlayer.getY(), 40, 40, null);
+		page.drawImage(myMonsterIcon.getImage(), myMonster.getX(), myMonster.getY(), 55, 55, null);
+		
+		//borders
+		page.setColor(Color.black);
+		page.fillRect(0, 0, 3, 797);//x y width height
+		page.fillRect(0, 0, 797, 3);
+		page.fillRect(797, 0, 3, 797);
+		page.fillRect(0, 797, 797, 3);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		if(arg0.getKeyCode() == KeyEvent.VK_D) {
-			myPlayer.setX(myPlayer.getX() + 20);
+			myPlayer.moveRight();
 		}
 		else if(arg0.getKeyCode() == KeyEvent.VK_A) {
-			myPlayer.setX(myPlayer.getX() - 20);
+			myPlayer.moveLeft();
 		}
 		else if(arg0.getKeyCode() == KeyEvent.VK_W) {
-			myPlayer.setY(myPlayer.getY() - 20);
+			myPlayer.moveUp();
 		}
 		else if(arg0.getKeyCode() == KeyEvent.VK_S) {
-			myPlayer.setY(myPlayer.getY() + 20);
+			myPlayer.moveDown();
 		}
-		
 		repaint();
-		
-		
 	}
 
 	@Override
@@ -60,5 +73,27 @@ public class Panel extends JPanel implements KeyListener{
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private class TimerListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			if(myPlayer.getX() > myMonster.getX()) {
+				myMonster.setX(myMonster.getX() + 20);
+			}
+			else if(myPlayer.getX() < myMonster.getX()) {
+				myMonster.setX(myMonster.getX() - 20);
+			}
+			if(myPlayer.getY() > myMonster.getY()) {
+				myMonster.setY(myMonster.getY() + 20);
+			}
+			else if(myPlayer.getY() < myMonster.getY()) {
+				myMonster.setY(myMonster.getY() - 20);
+			}
+			
+			repaint();
+		}
 	}
 }
