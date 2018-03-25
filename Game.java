b74949942b6d03d1852ab9620c454f131ada1d74
CodyandSam <37190239+CodyandSam@users.noreply.github.com>
2018-03-25 13:05:29 -0600

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,31 +1,30 @@
 package Game;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
 
 
 public class Game {
 	
 		
 		private int score;
-		private int highScores; // this is going to be an array
+		ArrayList highScores; // this is going to be an array
 		private static player myPlayer; // This will be the player object
-		private static Monster enemy; // This will be the enemy object
-		private static Item item; // This will be the item object
 		
 		
 		
-		public Game(int score, int highScores, player myPlayer, Monster enemy, Item item, player character) {
+		public Game(int score, ArrayList highScores) {
 			this.score = score;
 			this.highScores = highScores;
-			this.myPlayer = myPlayer;
-			this.enemy = enemy;
-			this.item = item;
-		
-			
+		}
+
+		public int getScore() {
+			return score;
+		}
+
+		public void setScore(int score) {
+			this.score = score;
 		}
 		
 }
